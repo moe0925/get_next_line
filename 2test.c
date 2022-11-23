b14641dc556f27413e1de0f5148f0f6cc2c33230
moeota <moe0925c5@gmail.com>
2022-11-24 08:30:52 +0900

    1124

--- 2test.c
+++ 2test.c
@@ -1,121 +1,122 @@
-// char *make_save()
-// {
-
-// }
-char *make_line(int fd ,char *save,int BUFFER_SIZE)
-{
-
-	char memo[256];
-	int	flag;
-	int i;
-	char *line;
-	i = 0;
-	if (fd < 0)
-		return (0);
-	fd = 0;
-	save = malloc (sizeof(char) * 1);
-	save[0] = 0;
-	read(fd,memo,BUFFER_SIZE);
-
-	while (i < BUFFER_SIZE)// ファイルの読み込みが終わるまで？？
-	{
-		if (check(memo[i]) == 1)//もしバッファサイズ分ごとに探している途中に改行が発見されたら、
-		{
-			printf("----------------");
-			memo[i] = 0;
-			line = malloc (sizeof(char) * (ft_strlen(save) + i + 1));
-			line = ft_strjoin(save, &memo[0]);
-			printf("%s\n",save);
-			save = &memo[i + 1];
-
-			return (line);
-			
-			// line2 = (char *)malloc((sizeof(char)) * ((i) + 1));
-			// if (!line2)
-			// 	return (0);
-			// printf("~~~~~~~~~~~~~~~~~~~line~~~~~~~~~~");
-
-			// line2 = ft_strjoin(save, &memo[0]);
-			// return (line2);
-
-			// printf("%s",line2);
-		}
-		save = &memo[0];
-		// else
-		// {
-		// 	save = malloc(sizeof(char) * (i + 2));
-		// 	save = ft_strjoin(save, &memo[i]);
-		// 	save = memo;
-		// 	printf("~~~~~~~~~~~~~~save~~~~~~~~~~~~~~\n");
-
-		// 	printf("%s\n",save);
-		// }
-		i++;
-	}
-	return (line);
-}
-
-
-
-
 int get_next_line(int fd, char **line)
 {
+	int read_byte;
+	char *memo;
 	int BUFFER_SIZE;
-	int	flag;
-	// char memo[256];
+	int i;
 	static char *save;
-	BUFFER_SIZE = 6;
-
-	*line = make_line(fd, save,BUFFER_SIZE);
-	for (int i = 0;i<4;i++)
-		printf("%c",line[0][i]);
+	save = malloc (sizeof(char) * 1);
+	save[0] = 0;
+	BUFFER_SIZE = 5;
+	char *line2;
+	line2 = *line;
+	char *temp;
+	int j;
+	j = 0;
 	
 
+	memo = malloc(sizeof(char) * BUFFER_SIZE + 1);
+	if (!memo)
+		return (0);
 	
-	// while ()
+	// printf("~~~~~~~~~~~~~~~~~~~~~test1~~~~~~~~~~\n");
+	read_byte = read(fd,memo, BUFFER_SIZE);
+	i = 0;
 
-	// read(fd,&memo, BUFFER_SIZE );
-	// flag = read(fd,&memo,BUFFER_SIZE );
-	// char *c = memo;
-	// for (int i = 0;i < 6;i++)
-	// printf("%s\n",&memo);
-	// printf("%c\n",memo[2]);
+	if (read_byte < 0)
+		return (0);
 
-	// printf("%d\n",flag);
-
-	// read(fd,&memo, BUFFER_SIZE);
-	// flag = read(fd,memo,BUFFER_SIZE);
-
+	while (read_byte > 0)
+	{
+		memo[BUFFER_SIZE + 1] = 0;
+		char *find;
 	
-	// while (//ファイルの読み込みが終わるまで)
-	// {
-	// buf1 = malloc (sizeof(char) * BUFFER_SIZE + 1);
-	// if (!buf1)
-	// 	return (0);
+		find = ft_strchr(memo, '\n');
+		if (save == 0)
+			save = &memo[0];
+		// printf("%s", find);
+		printf ("〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜テスト１〜〜〜〜〜〜〜〜〜\n%s\n",save);
+
+		while(ft_strchr(memo,'\n') != 0)
+		{
+			*find = 0;
+
+			j++;
+			printf("〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜%d回目の改行発見~~~~~~~~~~\n",j);
+			// printf("%s\n",save);
+			// printf("%s\n",memo);
+
+			line2 = (char *)malloc((sizeof(char))*((ft_strlen(save)) + (ft_strlen(memo)) + 1));
+			line2 = ft_strjoin(save,memo);
+			// printf ("%s",ft_strjoin(save,memo));
+			printf ("%s\n",line2);
+			save[0] = 0;
+			memo = find +1 ;
+			printf("現時点でのsave");
+			printf("%s\n", save);
+			printf("line: %s\n",line2);
+
+
+		}
+		// save = find + 1;
+		printf ("〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜テスト2〜〜〜〜〜〜〜〜〜\n%s\n",save);
+
+		
+		if (ft_strchr(memo,'\n')== 0)
+		{
+		printf("~~~~~~~~~~~~~~~~~~~~~改行なし~~~~~~~~~~\n");
+
+		// if (*save == 0 )
+		// 	save = &memo[0];
+		
+		temp = malloc(sizeof(char) * BUFFER_SIZE + 1);
+		temp = memo;
+		save = ft_strjoin(save,temp);
+		temp = 0;
+		
+		// printf ("%s\n",memo);
+		printf ("変更後%s\n",save);
+
+		// printf ("%p",save);
+		// for (int i = 0; i < BUFFER_SIZE ; i++)
+		i++;
+		printf("%d回目\n",i);
+		printf("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
+		printf ("最後%s\n",save);
+
+
+
+
+		}
+		
+
+		read_byte = read(fd,memo, BUFFER_SIZE);
+		// printf("~~~~~~~~~~~~再読み込み~~~~~~~~~~~~~~~~~~~\n");
+
+
+
+
+	}
 	
-	// flag = read(fd,buf1, BUFFER_SIZE);
-	// if (flag < 0)
-	// 	return 0;
+	// temp = malloc(sizeof(char) * (ft_strlen(save) + ft_strlen(memo) + 1));
+	// temp = memo;
+	// save = ft_strjoin(save,memo);
+	if (read_byte == 0)
+		printf("%s\n",save);	
+	printf ("000\n");
+	return (0);
 
-
-
-
-	// }
-	
-	
-	// // {
-
-	// // }
-	// for (int i = 0;i<1;i++)
-	
-	// save = malloc(sizeof(char) * BUFFER_SIZE + 1);
 	return (0);
 }
 
 
-int main(void)
+
+
+
+
+int main()
 {
-    // 変数定義
+// 変数定義
     int fd1;
     // char buf[BUF_SIZE];
 
@@ -139,53 +140,3 @@ int main(void)
     close(fd1);
     return 0;
 }
-
-/ int get_next_line(int fd)
-// {
-// 	int flag;
-// 	char memo[256];
-// 	// int BUFFER_SIZE;
-// 	int i;
-// 	char *save;
-// 	char *temp;
-// 	char **line;
-// 	// BUFFER_SIZE = 4;
-// 	flag = 0;
-// 	line = 0;
-// 	// printf("~~~~~~~~~~~~~~~~~~~~~test1~~~~~~~~~~\n");
-// 	// flag = read(fd,&memo[0], BUFFER_SIZE);
-// 	if (flag == -1)
-// 		return (0);
-// 	// if (flag == 1) //ファイルの終端にたどり着いた時
-// 	if (flag > 0)
-// 	{
-// 		// printf("~~~~~~~~~~~test2~~~~~~~~~~~~~~~~~~~~~~~\n");
-// 		flag = read(fd,&memo[0], BUFFER_SIZE);
-// 		// flag = read(fd,&memo[0], BUFFER_SIZE);
-// 		// flag = read(fd,&memo[0], BUFFER_SIZE);
-// 		i = 0;
-// 		while(i < BUFFER_SIZE)
-// 		{
-// 			// printf("~~~~~~~~~~~~~~~~~~~~~~test3~~~~~~~\n");
-
-// 			if (check(memo[i]) != 0)
-// 			{
-// 			// printf("---------------test4----------------------\n");
-
-// 				memo[i] = 0;//関数を使うために改行をヌルにする
-// 				temp = malloc(sizeof(char) * ((ft_strlen)(save) + i + 1 ));
-// 				temp = ft_strjoin(save , &memo[0]); 
-// 				*line = temp;
-// 				save = &memo[i + 1];//saveに残りの文字列の先頭ポインタを入れる
-// 				// printf("%s\n",temp);
-// 				return (0);
-// 			}
-// 			i++;
-
-// 		}
-
-// 	}
-// 	return (0);
-
-
-// }
--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: moeota <moeota@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/07 22:58:35 by moeota            #+#    #+#             */
-/*   Updated: 2022/11/23 20:25:22 by moeota           ###   ########.fr       */
+/*   Updated: 2022/11/24 08:28:52 by moeota           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,6 +33,12 @@ int get_next_line(int fd, char **line)
 	save = malloc (sizeof(char) * 1);
 	save[0] = 0;
 	BUFFER_SIZE = 5;
+	char *line2;
+	line2 = *line;
+	char *temp;
+	int j;
+	j = 0;
+	
 
 	memo = malloc(sizeof(char) * BUFFER_SIZE + 1);
 	if (!memo)
@@ -44,63 +50,71 @@ int get_next_line(int fd, char **line)
 
 	if (read_byte < 0)
 		return (0);
-	if (read_byte == 0)
-	{
-		// line2 = save;
-		// printf("%s\n", save);
-		printf ("0\n");
-		return (0);
-	}
+
 	while (read_byte > 0)
 	{
 		memo[BUFFER_SIZE + 1] = 0;
 		char *find;
-		char *line2;
-		line2 = *line;
+	
 		find = ft_strchr(memo, '\n');
-		// printf("%d", ft_strchr(memo, '\n') - memo[0]);
-
+		if (save == 0)
+			save = &memo[0];
 		// printf("%s", find);
+		printf ("〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜テスト１〜〜〜〜〜〜〜〜〜\n%s\n",save);
 
-		if(find != 0 )
+		while(ft_strchr(memo,'\n') != 0)
 		{
 			*find = 0;
-			printf("~~~~~~~~~~~~~~~~~~~~~改行発見~~~~~~~~~~\n");
-			printf("%s\n", save);
-			printf("%s\n", memo);
+
+			j++;
+			printf("〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜%d回目の改行発見~~~~~~~~~~\n",j);
+			// printf("%s\n",save);
+			// printf("%s\n",memo);
 
 			line2 = (char *)malloc((sizeof(char))*((ft_strlen(save)) + (ft_strlen(memo)) + 1));
 			line2 = ft_strjoin(save,memo);
 			// printf ("%s",ft_strjoin(save,memo));
 			printf ("%s\n",line2);
-			save = find + 1;
+			save[0] = 0;
+			memo = find +1 ;
 			printf("現時点でのsave");
 			printf("%s\n", save);
+			printf("line: %s\n",line2);
+
 
 		}
+		// save = find + 1;
+		printf ("〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜テスト2〜〜〜〜〜〜〜〜〜\n%s\n",save);
 
-		else
+		
+		if (ft_strchr(memo,'\n')== 0)
 		{
-			printf("~~~~~~~~~~~~~~~~~~~~~改行なし~~~~~~~~~~\n");
-			if (*save == 0)
-				save = &memo[0];
-			else
-			{
-				char *temp;
-				temp = malloc(sizeof(char) * BUFFER_SIZE + 1);
-				temp = memo;
-				save = ft_strjoin(save,temp);
-			}
-			
-			// printf ("%s\n",memo);
-			printf ("変更後%s\n",save);
+		printf("~~~~~~~~~~~~~~~~~~~~~改行なし~~~~~~~~~~\n");
 
-		}
+		// if (*save == 0 )
+		// 	save = &memo[0];
+		
+		temp = malloc(sizeof(char) * BUFFER_SIZE + 1);
+		temp = memo;
+		save = ft_strjoin(save,temp);
+		temp = 0;
+		
+		// printf ("%s\n",memo);
+		printf ("変更後%s\n",save);
+
 		// printf ("%p",save);
 		// for (int i = 0; i < BUFFER_SIZE ; i++)
 		i++;
 		printf("%d回目\n",i);
 		printf("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
+		printf ("最後%s\n",save);
+
+
+
+
+		}
+		
+
 		read_byte = read(fd,memo, BUFFER_SIZE);
 		// printf("~~~~~~~~~~~~再読み込み~~~~~~~~~~~~~~~~~~~\n");
 
@@ -108,62 +122,19 @@ int get_next_line(int fd, char **line)
 
 
 	}
-	printf ("0\n");
+	
+	// temp = malloc(sizeof(char) * (ft_strlen(save) + ft_strlen(memo) + 1));
+	// temp = memo;
+	// save = ft_strjoin(save,memo);
+	if (read_byte == 0)
+		printf("%s\n",save);	
+	printf ("000\n");
+	return (0);
 
 	return (0);
 }
 
 
-
-// int get_next_line(int fd)
-// {
-// 	int flag;
-// 	char *memo;
-// 	int BUFFER_SIZE;
-// 	int i;
-// 	char *save;
-// 	char *temp;
-// 	char **line;
-// 	BUFFER_SIZE = 4;
-// 	line = 0;
-// 	memo = malloc(sizeof(char) * BUFFER_SIZE + 1);
-// 	// printf("~~~~~~~~~~~~~~~~~~~~~test1~~~~~~~~~~\n");
-// 	flag = read(fd,&memo[0], BUFFER_SIZE);
-// 	if (flag == -1)
-// 		return (0);
-// 	// if (flag == 1) //ファイルの終端にたどり着いた時
-// 	if (flag > 0)
-// 	{
-// 		// printf("~~~~~~~~~~~test2~~~~~~~~~~~~~~~~~~~~~~~\n");
-// 		flag = read(fd,&memo[0], BUFFER_SIZE);
-// 		// flag = read(fd,&memo[0], BUFFER_SIZE);
-// 		// flag = read(fd,&memo[0], BUFFER_SIZE);
-// 		i = 0;
-// 		while(i < BUFFER_SIZE)
-// 		{
-// 			// printf("~~~~~~~~~~~~~~~~~~~~~~test3~~~~~~~\n");
-
-// 			if (check(memo[i]) != 0)
-// 			{
-// 			// printf("---------------test4----------------------\n");
-
-// 				memo[i] = 0;//関数を使うために改行をヌルにする
-// 				temp = malloc(sizeof(char) * ((ft_strlen)(save) + i + 1 ));
-// 				temp = ft_strjoin(save , &memo[0]); 
-// 				*line = temp;
-// 				save = &memo[i + 1];//saveに残りの文字列の先頭ポインタを入れる
-// 				// printf("%s\n",temp);
-// 				return (0);
-// 			}
-// 			i++;
-
-// 		}
-
-// 	}
-// 	return (0);
-
-
-// }
 
 
 
