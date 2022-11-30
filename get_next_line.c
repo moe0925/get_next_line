f4982579baaee9457d97a48244a03729182fe2d9
moeota <moe0925c5@gmail.com>
2022-11-30 23:52:03 +0900

    latest

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: moeota <moeota@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/07 22:58:35 by moeota            #+#    #+#             */
-/*   Updated: 2022/11/30 21:33:28 by moeota           ###   ########.fr       */
+/*   Updated: 2022/11/30 22:04:26 by moeota           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -64,33 +64,24 @@ char *get_next_line(int fd)
 	if (!memo)
 		return (0);
 	read_byte = read(fd,memo, BUFFER_SIZE);
-	// printf("savenew :%s\n", save);
-
-	// printf("memo :%s\n", memo);
+	
 	if (read_byte < 0)
 		return (0);
 
 	while (read_byte > 0)
 	{
-		// if (save == 0)
-		// 	save = &memo[0];
+		find = check(memo);
 
 		while(check(memo) != 0 || memo[0] == '\n')
 		{
 			
-			find = check(memo);
 			
 			temp1 = malloc(sizeof(char) * (find + 1));
 			ft_strlcpy(temp1,memo, (find +1));
 			line = (char *)malloc((sizeof(char))*((ft_strlen(save)) + (ft_strlen(temp1)) + 1));
 			line = ft_strjoin(save,temp1);
-			// printf("save :%s\n",save);
 			free(temp1);
-			// free(save);
-			// if (save)
-			// 	free(save);
-			// save[0] = 0;
-			// save = &memo[find + 1]; 
+	 		free(save);
 			save = malloc(sizeof(char) * (BUFFER_SIZE - find));
 			int i;
 			i = 0;
@@ -101,44 +92,33 @@ char *get_next_line(int fd)
 			}
 			save[i] = 0;
 			free(memo);
-			// printf("----------------------save :%s---------------------\n",save);
-
 			return (line);
 		}
 
-		if (check(memo)== 0 && memo[0] != '\n')
+		if ((find == 0) && memo[0] != '\n')
 		{
-			// printf("----------------------ここに１が入る-------------- :%s\n",save);
 			temp1 = ft_strdup(memo);
-			// printf("save3:%s\n",save);
-			// printf("temp1:%s\n",temp1);
 			save = ft_strjoin(save,temp1);
-			// printf("save2 :%s\n",save);
 			free(temp1);
+			
+			// free(memo);
 		}
-		
-		// free(memo);//ここおかしい
+		///ここでフリー絶対しない！！！
+		// free(memo);
+
 		read_byte = read(fd,memo, BUFFER_SIZE);
 	}
 	
 	if (read_byte == 0 && ((save[0] != 0)))
 		{
 			line = ft_strdup(save);
-			save[0] = 0;
-			// malloc_free(save);
+			free(save);
+			// free(memo);
+			// save[0] = 0;
 			return (line);
 		}
-	// if (read_byte == 0 && (save))
-	// line = ft_strdup(save);
-	// return (line);
-	// 	printf("%s",save);
-	// {
-	// 	printf("-----------%s\n",save);	
-	// printf("~~~~~~~~~~~~^%s\n",line);
-	// save[0] = 0;
-	// 	return (line);
-	// }
-	// return (line);
+			// free(memo);
+
 	return (NULL);
 }
 
