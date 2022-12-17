b636ab8abe2715a2bed03fd75334786d8507d794
moeota <moe0925c5@gmail.com>
2022-12-17 11:15:45 +0900

    1111

--- get_next_line.c
+++ get_next_line.c
@@ -10,6 +10,38 @@
 
 int BUFFER_SIZE = 5;
 
+int check(char *str);
+
+
+char *make_line(char *memo,char *save)
+{
+	int find;
+	char *temp1;
+	char *line;
+	find = check(memo);
+	temp1 = malloc(sizeof(char) * (find + 1));
+	// system("leaks a.out");
+	ft_strlcpy(temp1,memo, (find +1));
+	line = (char *)malloc((sizeof(char))*((ft_strlen(save)) + (ft_strlen(temp1)) + 1));
+	if (!line)
+		return (NULL);
+	line = ft_strjoin(save,temp1);
+	free(temp1);
+	free(save);
+	save = malloc(sizeof(char) * (BUFFER_SIZE - find - 1));
+	int i;
+	i = 0;
+	while (memo[find + i + 1] != '\0')
+	{
+		save[i] = memo[find + i + 1];
+		i++;
+	}
+	save[i] = '\0';
+	free(memo);//いる
+	return (line);
+
+}
+
 
 
 int check(char *str)
@@ -23,21 +55,21 @@ int check(char *str)
 			return (i);
 		i++;
 	}
-	return (0);
+	return (-1);
 }
 
-char *malloc_free(char *line)
-{
-	int i;
-	i = 0;
-	while (!(line))
-	{
-		free(line);
-		i++;
-	}
-	line = ft_calloc(1,sizeof(char));
-	return (line);
-}
+// char *malloc_free(char *line)
+// {
+// 	int i;
+// 	i = 0;
+// 	while (!(line))
+// 	{
+// 		free(line);
+// 		i++;
+// 	}
+// 	line = ft_calloc(1,sizeof(char));
+// 	return (line);
+// }
 
 char *get_next_line(int fd)
 {
@@ -45,64 +77,84 @@ char *get_next_line(int fd)
 	char *memo;
 	char *line;
 	char *temp1;
-	int find;
 	static char *save;
 
-	if (!save)
-		save = ft_calloc(1,sizeof(char));
+	printf("readbyte:%d\n",read_byte);
+	printf("memo:%s\n",memo);
+	// if (read_byte < 0)
+	// {
+	// 	free(save);
+	// 	return (NULL);
+	// }
+	printf("check:%d\n",check(memo));
 
-	memo = ft_calloc(BUFFER_SIZE+ 1, sizeof(char));
-	if (!memo)
-		return (NULL);
-	read_byte = read(fd, memo, BUFFER_SIZE);
+	read_byte = 1;
 
-	if (read_byte < 0)
-		return (NULL);
-	
-	if (read_byte > 0)
+	while (read_byte != 0)
 	{
-		if (check(memo) != 0 || memo[0] == '\0')
+		if (!save)
+			save = ft_calloc(1,sizeof(char));
+
+		memo = ft_calloc(BUFFER_SIZE+ 1, sizeof(char));
+		if (!memo)
 		{
-			printf("-------------------------------------");
-			find = check(memo);
-			temp1 = malloc(sizeof(char) * (find + 1));
-			ft_strlcpy(temp1,memo, (find +1));
-			line = (char *)malloc((sizeof(char))*((ft_strlen(save)) + (ft_strlen(temp1)) + 1));
-			if (!line)
-				return (NULL);
-			line = ft_strjoin(save,temp1);
-			free(temp1);
-	 		free(save);
-			save = malloc(sizeof(char) * (BUFFER_SIZE - find));
-			int i;
-			i = 0;
-			while (memo[find + i + 1] != '\0')
-			{
-				save[i] = memo[find + i + 1];
-				i++;
-			}
-			save[i] = 0;
-			free(memo);//いる
-			return (line);
+			free(save);
+			return (NULL);
 		}
 
-		else if(check(memo) == 0 && memo[0] != '\n')
+		read_byte = read(fd, memo, BUFFER_SIZE);
+
+		// if (read_byte < 0)
+		// {
+		// 	free(save);
+		// 	free(memo);
+		// 	return(NULL);
+		// }
+
+		if (check(memo) > 0/* || memo[0] == '\0' */)
 		{
-			temp1 = ft_strdup(memo);
-			save = ft_strjoin(save,temp1);
-			free(temp1);
-			free(memo);
+			return((make_line(memo,save)));
 		}
-		// free(memo);
-
-	}
-
-	if (read_byte == 0 && ((save[0] != 0)))
+		else if (check(memo) == 0)
 		{
 			line = ft_strdup(save);
+		// line = ft_strjoin(save,memo);
 			free(save);
+			free(memo);
 			return (line);
 		}
+
+		else if(check(memo) == -1)
+		{
+			temp1 = ft_strdup(memo);
+			free(memo);
+			save = ft_strjoin(save,temp1);
+			free(temp1);
+			printf("save:%s\n",save);
+		}
+
+	}
+	// if  (read_byte > 0)
+	// {
+		
+		
+	// }
+	// printf("------hi---");
+
+	// if (read_byte == 1 )
+	// {
+	// 	// printf("save2:%s\n",save);
+	// 	line = ft_strdup(save);
+	// 	// line = ft_strjoin(save,memo);
+	// 	// printf("---------");
+	// 	free(save);
+	// 	free(memo);
+	// 	return (line);
+	// }
+	// // free(save);
+	// if (read_byte == 0)
+	// 	return (save);
+	printf("readbyte:%d\n",read_byte);
 	// free(save);
 	return (NULL);
 }
@@ -138,9 +190,10 @@ int main()
 	check++;
 	free(line);
 	}
-	system("leaks a.out");
    
     // ファイルを閉じる
     close(fd1);
+	// system("leaks a.out");
+
     return 0;
 }
